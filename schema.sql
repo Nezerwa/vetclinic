86ac965f747a73ca5c3613889ee7b0087e50229a
Nezerwa <eligrand2000@gmail.com>
2023-04-08 16:08:17 +0200

    add data of added tables

--- schema.sql
+++ schema.sql
@@ -29,27 +29,3 @@ ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
-
-/*"join table" for visits*/
-CREATE TABLE vets (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name TEXT,
-  age INTEGER,
-  date_of_graduation DATE
-);
-
-/*species and vets many-to-many relationship */
-CREATE TABLE specializations (
-  vet_id INTEGER REFERENCES vets(id),
-  species_id INTEGER REFERENCES species(id),
-  PRIMARY KEY (vet_id, species_id)
-);
-ALTER TABLE animals ADD CONSTRAINT animals_id_unique UNIQUE (id);
-
-/*animals and vets many-to-many relationship */
-CREATE TABLE visits (
-    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    animals_id integer REFERENCES animals(id),
-    vet_id integer REFERENCES vets(id),
-    visit_date DATE
-);
